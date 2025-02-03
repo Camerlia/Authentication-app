58e03c15fdb485313d55c5c4f212ed49be002eae
Camelia <Cameliaafolabi@gmail.com>
2025-02-03 23:14:35 +0100

    update development and modified packages

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,31 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
+import path from "path";
+dotenv.config();
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
-app.use(express.json());
-app.use(cookieParser())
-// Connect to the database before starting the server
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log("server is runing on port", PORT)
-})
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port: ", PORT);
+});
